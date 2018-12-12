5784efdc7aeaa42aececcc8277682916293a7549
Your Name <rogerpuichungyau@gmail.com>
2018-12-12 15:02:36 +0800

    vuetify nav drawer added

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,46 @@
 <template>
-  <v-app>
+  <v-app style="background: #E3E3EE">
     <!-- side navbar -->
     <v-navigation-drawer v-model="sideNav" fixed temporary app>
-      <v-toolbar>
-        <v-list>
-          
-        </v-list>
+      <v-toolbar color="accent" dark flat>
+        <v-toolbar-side-icon @click="drawerToggle"></v-toolbar-side-icon>
+        <router-link to="/" tag="span" style="cursor:pointer">
+          <h1 pl-5>VueShare</h1>
+        </router-link>
       </v-toolbar>
-      <v-btn @click="drawerToggle">toggle</v-btn>
+
+      <!-- invisible divider -->
+      <v-divider></v-divider>
+
+      <!-- side navbar links -->
+      <v-list>
+        <v-list-tile v-for="item in sideBarNavItems" :key="item.key" :to="item.link">
+          <v-list-tile-action>
+            <v-icon>{{item.icon}}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            {{item.title}}
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+
     </v-navigation-drawer>
     
 
 
     <!-- horizontal nav bar -->
-    <v-toolbar fixed color="primary" dark>
+    <v-toolbar fixed color="primary" dark class="">
       <!-- app title -->
       <v-toolbar-side-icon @click="drawerToggle"></v-toolbar-side-icon>
 
-      <v-toolbar-title class="hidden-sm-only">
+      <v-toolbar-title class="">
         <router-link to="/" tag="span" style="cursor:pointer">
           VueShare
         </router-link>
       </v-toolbar-title>
 
 
-      <v-spacer></v-spacer>
+      <v-spacer class=""></v-spacer>
       
       <!-- search input -->
       <v-text-field
@@ -34,6 +50,7 @@
        color="accent"
        single-line
        hide-details
+       class="hidden-xs-only"
        ></v-text-field>
 
 
@@ -45,9 +62,10 @@
           v-for="item in horizontalNavItems" 
           :key="item.title" 
           :to="item.link"
+          class="hidden-sm-and-down"
         >
           <v-icon left>{{item.icon}}</v-icon>
-          <div class="hidden-sm-only">{{item.title}}</div>
+          <div class="hidden-md-and-down">{{item.title}}</div>
         </v-btn>
       </v-toolbars-items>
 
@@ -73,6 +91,13 @@ export default {
   name:'App',
   computed:{
     horizontalNavItems(){
+      return [
+        {icon:'chat',title:'Posts',link:'/posts'},
+        {icon:'lock_open',title:'Sign In',link:'/signin'},
+        {icon:'create',title:'Sign Up',link:'/signup'},
+      ]
+    },
+    sideBarNavItems(){
       return [
         {icon:'chat',title:'Posts',link:'/posts'},
         {icon:'lock_open',title:'Sign In',link:'/signin'},
